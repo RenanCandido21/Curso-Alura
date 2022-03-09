1b34050fb4a4fbba354f2110fa9bada169dbfc21
RenanCandido21 <101265494+RenanCandido21@users.noreply.github.com>
2022-03-09 11:18:13 -0300

    Delete 1-ternario.js

--- 1-ternario.js
+++
@@ -1,8 +0,0 @@
-const idadeMinima = 18
-const idadeCliente = 15
-
-
-                  //condição               //true       //false
-console.log(idadeCliente>= idadeMinima ? "cerveja" : "suco")
-//Tem que tomar cuidado para não criar ternario dentro de ternario, 
-//Caso eu queira fazer isso é recomendado usar o If.
