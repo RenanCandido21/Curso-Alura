f8fc2c93884185f724b29fa14b849dbe98519449
RenanCandido21 <101265494+RenanCandido21@users.noreply.github.com>
2022-03-09 11:19:01 -0300

    Delete 5-ex1Array.js

--- 5-ex1Array.js
+++
@@ -1,9 +0,0 @@
-//Exercicio JS - Reprovado ou Aprovado?
-
-const nomes = [' Renan ', ' Maria ', ' Angelica ', ' Lohan ', ' Rayane ', ' Josivaldo ']
-
-const notas = [ ' 10 ', ' 8 ', ' 4 ', ' 5.5 ', ' 9 ', ' 10 ']
-
-const reprovados = nomes.filter ( (alunos,indice) => notas [indice] < 7)
-
-console.log (`reprovados: ${reprovados}`)
